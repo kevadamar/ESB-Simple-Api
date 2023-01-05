e854b8fa234097f8e766fd97a01c04ecf2d74c62
kevadamar <kevadamarg@gmail.com>
2023-01-06 02:25:31 +0700

    update(Invoices): Fix bug invoice

--- app/Services/TBillingInvoicesService.php
+++ app/Services/TBillingInvoicesService.php
@@ -22,13 +22,13 @@ class TBillingInvoicesService extends BaseService
     public function list(Request $request)
     {
         $page = $request->size ?? 10;
-        $with = ['from_destination:id,code,name,description', 'to_destination:id,code,name,description', 'detail:id,t_billing_invoice_id,m_service_id,quantity,total_subamount', 'detail.service:id,m_service_type_id,code,description,unit_price','detail.service.service_type:id,code,description'];
+        $with = ['from_destination:id,code,name,description', 'to_destination:id,code,name,description', 'detail:id,t_billing_invoice_id,m_service_id,quantity,total_subamount', 'detail.service:id,m_service_type_id,code,description,unit_price', 'detail.service.service_type:id,code,description'];
         return $this->tBillingInvoices->with($with)->filter($request)->orderBy('id', 'desc')->paginate($page);
     }
 
     public function get($id)
     {
-        $with = ['from_destination:id,code,name,description', 'to_destination:id,code,name,description', 'detail:id,t_billing_invoice_id,m_service_id,quantity,total_subamount', 'detail.service:id,m_service_type_id,code,description,unit_price','detail.service.service_type:id,code,description'];
+        $with = ['from_destination:id,code,name,description', 'to_destination:id,code,name,description', 'detail:id,t_billing_invoice_id,m_service_id,quantity,total_subamount', 'detail.service:id,m_service_type_id,code,description,unit_price', 'detail.service.service_type:id,code,description'];
         return $this->tBillingInvoices->with($with)->find($id);
     }
 
@@ -54,11 +54,12 @@ class TBillingInvoicesService extends BaseService
         foreach ($payload['detail'] as $key => $value) {
             $subTotalAmount = $value['price'] * $value['quantity'];
 
-            $payloadInvoiceDetails['m_service_id'] = $value['m_service_id'];
-            $payloadInvoiceDetails['quantity'] = $value['quantity'];
-            $payloadInvoiceDetails['total_subamount'] = $subTotalAmount;
-            $payloadInvoiceDetails['created_at'] = Carbon::now();
-            $payloadInvoiceDetails['updated_at'] = Carbon::now();
+            $temp['m_service_id'] = $value['m_service_id'];
+            $temp['quantity'] = $value['quantity'];
+            $temp['total_subamount'] = $subTotalAmount;
+            $temp['created_at'] = Carbon::now();
+            $temp['updated_at'] = Carbon::now();
+            array_push($payloadInvoiceDetails, $temp);
 
             $totalAmount += $subTotalAmount;
         }
@@ -71,7 +72,7 @@ class TBillingInvoicesService extends BaseService
         $store = $this->save($this->tBillingInvoices, $payloadInvoices);
 
         foreach ($payloadInvoiceDetails as $key => $value) {
-            $payloadInvoiceDetails['t_billing_invoice_id'] = $store->id;
+            $payloadInvoiceDetails[$key]['t_billing_invoice_id'] = $store->id;
         }
 
         $store['detail'] = $payloadInvoiceDetails;
@@ -90,18 +91,25 @@ class TBillingInvoicesService extends BaseService
         $payloadInvoiceDetails = [];
 
         $totalAmount = 0;
+        $this->destroyBulk($this->tBillingInvoiceDetails, 't_billing_invoice_id', $id);
+
         foreach ($payload['detail'] as $key => $value) {
             $subTotalAmount = $value['price'] * $value['quantity'];
 
-            $payloadInvoiceDetails['m_service_id'] = $value['m_service_id'];
-            $payloadInvoiceDetails['quantity'] = $value['quantity'];
-            $payloadInvoiceDetails['total_subamount'] = $subTotalAmount;
-            $payloadInvoiceDetails['created_at'] = Carbon::now();
-            $payloadInvoiceDetails['updated_at'] = Carbon::now();
+            $temp['m_service_id'] = $value['m_service_id'];
+            $temp['quantity'] = $value['quantity'];
+            $temp['total_subamount'] = $subTotalAmount;
+            $temp['created_at'] = Carbon::now();
+            $temp['updated_at'] = Carbon::now();
+            $temp['t_billing_invoice_id'] = $id;
 
-            $this->update($this->tBillingInvoiceDetails, $payloadInvoiceDetails, $value['id']);
+            array_push($payloadInvoiceDetails, $temp);
 
             $totalAmount += $subTotalAmount;
+        }
+
+        if (!$this->saveBulk($this->tBillingInvoiceDetails, $payloadInvoiceDetails)) {
+            throw new Exception("Failed Save Invoices. Please Contact Administrator", 500);
         }
 
 
